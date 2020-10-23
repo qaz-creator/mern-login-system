55b3fab1bbba71779ead378b7307126c1965eb2e
qaz-creator <codewarcode@protonmail.com>
2020-10-23 12:06:00 -0400

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const env = require('dotenv')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+mongoose.connect(
+  process.env.MONGO_DB_URI ||
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.o6isb.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+)
+
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api', require('./routes/index'))
+const port = process.env.PORT || 5000
+
+
+if(process.env.NODE_ENV==='production'){
+app.use(express.static('frontend/build'))
+}
+app.listen(port, console.log(`Server is starting at Port ${port}`))
