15c760d4be79ef710502273f3c58686b6874df8c
qaz-creator <codewarcode@protonmail.com>
2020-10-23 12:16:15 -0400

    change the port

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ const env = require('dotenv')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-mongoose.connect(
-  process.env.MONGO_DB_URI ||
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.o6isb.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-)
+env.config()
+mongoose
+  .connect(
+    process.env.MONGO_DB_URI ||
+      `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.o6isb.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  )
+  .then(() => {
+    console.log('Database connected')
+  })
 
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', require('./routes/index'))
-const port = process.env.PORT || 5000
+const port = process.env.PORT | 2000
 
-
-if(process.env.NODE_ENV==='production'){
-app.use(express.static('frontend/build'))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'))
 }
 app.listen(port, console.log(`Server is starting at Port ${port}`))
